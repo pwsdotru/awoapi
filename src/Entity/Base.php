4a08e8f6b587eec0e938746b7604ca363dd72d55
Aleksandr Novikov <pwsdotru@gmail.com>
2022-11-24 22:35:43 +0300

    Метод для инициализации полей объектов

--- src/Entity/Base.php
+++ src/Entity/Base.php
@@ -9,5 +9,34 @@ namespace AwoAPI\Entity;
  */
 class Base
 {
+    public function setData(array $data): self
+    {
+        foreach ($data as $key => $value) {
+            $name = $this->getPropertyName($key);
+            if ($this->isProperty($name)) {
+                $this->$name = $value;
+            }
+        }
+        return $this;
+    }
 
+    /**
+     * Перевод имени из snake в camel case
+     * @param string $name
+     * @return string
+     */
+    protected function getPropertyName(string $name): string
+    {
+        return lcfirst(implode('', array_map("ucfirst", explode('_', $name))));
+    }
+
+    /**
+     * Проверяет есть в классе свойство с заданым именем или нет
+     * @param string $name
+     * @return bool
+     */
+    protected function isProperty(string $name): bool
+    {
+        return property_exists($this, $name);
+    }
 }
